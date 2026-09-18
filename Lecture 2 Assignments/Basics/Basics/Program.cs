3ca5bf725bc6fb2949637df5f24cce521ce91075
Moataz Magdy <mmagdy@getgroup.com>
2026-09-19 02:55:26 +0300

    Assignment 1 : Solution of Question 2

--- Lecture 2 Assignments/Basics/Basics/Program.cs
+++ Lecture 2 Assignments/Basics/Basics/Program.cs
@@ -6,8 +6,15 @@
         static void Main(string[] args)
         {
             #region Solution of Question 1 
-            object newBook = new Book();
-            Console.WriteLine(newBook);
+            Book newBook = new Book();
+            //Console.WriteLine(newBook);
+            #endregion
+
+            #region Solution of Question 2
+            Console.WriteLine(newBook.ToString());
+            Console.WriteLine(newBook.Equals(newBook));
+            Console.WriteLine(newBook.GetHashCode());
+            Console.WriteLine(newBook.GetType());
             #endregion
         }
     }
@@ -15,8 +22,20 @@
 
     class Book
     {
-        string Title ;
-        int Pages ;
+        public string Title;
+        public int Pages ;
+
+        public override string ToString()
+        {
+            return $"Book Title: {Title}, Number of Pages: {Pages}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Book anotherBook = (Book) obj;
+
+            return this.Title == anotherBook.Title && this.Pages == anotherBook.Pages ;
+        }
     }
 
 }
