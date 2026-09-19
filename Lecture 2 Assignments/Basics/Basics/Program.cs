b01acc3d8302f8f9b02259de519208bf4ad671bc
Moataz Magdy <mmagdy@getgroup.com>
2026-09-19 03:11:02 +0300

    Cleaning Branch to inital State

--- Lecture 2 Assignments/Basics/Basics/Program.cs
+++ Lecture 2 Assignments/Basics/Basics/Program.cs
@@ -5,36 +5,7 @@
         
         static void Main(string[] args)
         {
-            #region Solution of Question 1 
-            Book newBook = new Book();
-            //Console.WriteLine(newBook);
-            #endregion
-
-            #region Solution of Question 2
-            Console.WriteLine(newBook.ToString());
-            Console.WriteLine(newBook.Equals(newBook));
-            Console.WriteLine(newBook.GetHashCode());
-            Console.WriteLine(newBook.GetType());
-            #endregion
-        }
-    }
-
-
-    class Book
-    {
-        public string Title;
-        public int Pages ;
-
-        public override string ToString()
-        {
-            return $"Book Title: {Title}, Number of Pages: {Pages}";
-        }
-
-        public override bool Equals(object? obj)
-        {
-            Book anotherBook = (Book) obj;
-
-            return this.Title == anotherBook.Title && this.Pages == anotherBook.Pages ;
+           
         }
     }
 
